6463a3b7afdb265185d0699e483dc5eb5ab4e4da
lynakimhay <nakimhay.ly@institute.pse.ngo>
2024-12-04 15:55:43 +0700

    update

--- controllers/studentsController.js
+++ controllers/studentsController.js
@@ -28,12 +28,15 @@ const createstudents = async (req,res) => {
     
 };
 
-const allStudents = async () => {
+const allStudents = async (req,res) => {
     try {
         const students = await Students.find();
+        res.status(200).json({ success: true, data: students });
         console.log("All students:", students);
     } catch (err) {
         console.error("Error reading students:", err);
+        res.status(200).json({ success: false, message:err.message  });
+
     }
 };
 
@@ -44,12 +47,12 @@ const StudentsById = async (req,res) => {
             return res.status(404).json({ message: "Student not found" });
         }
         const students = await Students.findById(id);
-        if (students) {
-            console.log("student Found:", students);
-        } else {
-            console.log("student not found.");
-        }
+        if (!students) {
+            return res.status(404).json({ success: false, message: `student with ID ${id} not found` });
+        } 
+        res.status(200).json({ success: true, data: students });
     } catch (err) {
+        res.status(500).json({ success: false, message:err.message  });
         console.error("Error reading student:", err);
     }
 };
@@ -59,16 +62,16 @@ const updateStudents = async (req,res) => {
         const {id}=req.params;
         const {name,email,phone,classId}=req.body;
         if (!mongoose.Types.ObjectId.isValid(id)){
-            return res.status(404).json({ message: "Student not found" });
+            return res.status(400).json({ success:false,message: "Student not found" });
         }
-        const updatedStudents = await Students.findByIdAndUpdate(name,email,phone,classId, { new: true });
-        if (updatedStudents) {
-            console.log("student Updated:", updatedStudents);
-        } else {
-            console.log("student not found for update.");
-        }
+        const updatedStudents = await Students.findByIdAndUpdate(
+           id,{ name,email,phone,classId}, { new: true });
+        if (!updatedStudents) {
+            return res.status(404).json ({success:false,message:`students With ID ${id}`})
+        } 
+        res.status(200).json({success:true,data:updatedStudents});
     } catch (err) {
-        console.error("Error updating student:", err);
+        res.status(500).json({ success: false, message:err.message  });
     }
 };
 
@@ -76,16 +79,15 @@ const deleteStudents = async (req,res) => {
     try {
         const {id} = req.params;
         if (!mongoose.Types.ObjectId.isValid(id)){
-            return res.status(404).json({ message: "Student not found" });
+            return res.status(400).json({ message: "Student not found" });
         }
         const deleteStudents = await Students.findByIdAndDelete(id);
-        if (deleteStudents) {
-            console.log("student Deleted:", deleteStudents);
-        } else {
-            console.log("student not found for deletion.");
+        if (!deleteStudents) {
+            return res.status(404).json({ success: false, message: `students with ID ${id} not found` });
         }
+        res.status(200).json({ success: true, message: `students with ID ${id} has been deleted` });
     } catch (err) {
-        console.error("Error deleting student:", err);
+        res.status(500).json({ success: false, message: err.message });
     }
 };
 
